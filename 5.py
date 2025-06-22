dc0754eaa32270671a56859f86c23e0068ade135
kartikkatti100@gmail.com <kartikkatti100@gmail.com>
2025-06-22 22:21:34 +0530

    war

--- 5.py
+++ 5.py
@@ -5,3 +5,6 @@
 '4. for multiword variable ,need to follow camelcase rule i.e everynew word except first start with capital letter'
 '5. for multiword variable ,can follow parallel case rule i.e from start all words starts with capital letters'
 '6. for multiword variable, can use _ to separate the words'
+
+
+# Israel has accused Iran of harboring genocidal intentions, while Iran has accused Israel of conducting a genocide in Gaza. Consequently, Israel has sought sanctions and military action against Iran to stop it from acquiring nuclear weapons.
