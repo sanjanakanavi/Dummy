c76cdb8a500047045060c962f8069458a918ad34
sanjanakanavi <sanjanakanavi2000@gmail.com>
2025-06-22 21:50:50 +0530

    first

---
+++ 1.py
@@ -0,0 +1,10 @@
+print("hello world")
+print()
+print("sanjana")
+print()
+print("print('what are you doing')")
+print()
+print("hello world \nhello world \nhello world")
+print()
+print("Sanjana"+" Kanavi")
+'all about print function'
